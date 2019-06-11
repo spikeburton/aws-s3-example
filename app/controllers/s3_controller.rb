9fa62d9652cff64e875e3c088390dfa053ce28e3
spikeburton <ralphburton3@gmail.com>
2019-06-11 16:09:20 -0400

    add direct_post action

--- app/controllers/s3_controller.rb
+++ app/controllers/s3_controller.rb
@@ -1,5 +1,6 @@
 class S3Controller < ApplicationController
   def direct_post
-    render json: { msg: "Hello, World" }, status: :ok
+    data = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+    render json: { url: data.url, fields: data.fields }, status: :ok
   end
 end
